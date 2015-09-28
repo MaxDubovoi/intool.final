e805380e7682d7408942cfacc620eb3de42d306f
Max <maximdubovoi@gmail.com>
2015-09-28 18:49:19 +0300

    History&Aim

--- views/elements/_header.php
+++ views/elements/_header.php
@@ -7,7 +7,7 @@ session_start();
     <a href=""><img class="logo" src="images/logo.png" alt=""></a>
     <h3>ВАШ  НАДЕЖНЫЙ  ПАРТНЕР  В  ОБЛАСТИ  МЕТАЛЛООБРАБОТКИ</h3>
     <div class="lang-panel">
-        <i class="message-icon"></i>
+        <a href="mailto:maximdubovoi@gmail.com" class="message-icon"></a>
         <ul class="site-lang-list">
             <li class="lang-item"><span>Rus</span></li>
             <li class="lang-item"><a href="">Eng</a></li>
--- views/elements/_right-side.php
+++ views/elements/_right-side.php
@@ -1,5 +1,5 @@
 <ul class="partners">
-    <li><a href="https://vk.com/feed" target="_blank"><i class="icon mitsubishi"></i> </a></li>
+    <li><a href="" target="_blank"><i class="icon mitsubishi"></i> </a></li>
     <li><a href="" target="_blank"><i class="icon singular"></i> </a></li>
     <li><a href="" target="_blank"><i class="icon winstar"></i> </a></li>
     <li><a href="" target="_blank"><i class="icon creamtec"></i> </a></li>
