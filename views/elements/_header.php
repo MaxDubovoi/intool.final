c1f3d4f6ecb7bdd7847d5dd994a637102ec7d265
Max <maximdubovoi@gmail.com>
2015-10-31 15:40:16 +0200

    fonts problem

--- views/elements/_header.php
+++ views/elements/_header.php
@@ -3,7 +3,6 @@ session_start();
 
 
 ?>
-<div class="header">
     <a href=""><img class="logo" src="images/logo.png" alt=""></a>
     <h3>ВАШ  НАДЕЖНЫЙ  ПАРТНЕР  В  ОБЛАСТИ  МЕТАЛЛООБРАБОТКИ</h3>
     <div class="lang-panel">
@@ -13,4 +12,3 @@ session_start();
             <li class="lang-item"><a href="">Eng</a></li>
         </ul>
     </div>
-</div>
