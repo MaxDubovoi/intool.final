640b3a9ab126124eff0d5c5eaabfa1dd553aa6bc
Max <maximdubovoi@gmail.com>
2015-11-05 22:06:23 +0200

    mailto

--- views/elements/_header.php
+++ views/elements/_header.php
@@ -6,7 +6,7 @@ session_start();
     <a href=""><img class="main-logo" src="images/logo.png" alt=""></a>
     <h3>ВАШ  НАДЕЖНЫЙ  ПАРТНЕР  В  ОБЛАСТИ  МЕТАЛЛООБРАБОТКИ</h3>
     <div class="lang-panel">
-        <a href="mailto:maximdubovoi@gmail.com" class="message-icon"></a>
+        <a href="mailto:bogomol@intool.com.ua" class="message-icon"></a>
         <ul class="site-lang-list">
             <li class="lang-item"><span>Rus</span></li>
             <li class="lang-item"><a href="">Eng</a></li>
