517661bb1d83b1506207504eba67d5357918e75f
Max <maximdubovoi@gmail.com>
2016-01-31 13:03:03 +0200

    hide Yandex informer add alt on pages

--- views/elements/_right-side.php
+++ views/elements/_right-side.php
@@ -7,7 +7,9 @@
     <li><a href="http://www.eroglumakina.com/" target="_blank"><i class="icon eroglu"></i> </a></li>
 </ul>
 <!-- Yandex.Metrika informer -->
+<!--
 <a href="https://metrika.yandex.ru/stat/?id=34716185&amp;from=informer"
    target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/34716185/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
                                        style="width:88px; height:31px; border:0; margin-top:20px" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
+-->
 <!-- /Yandex.Metrika informer -->
